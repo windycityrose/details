eb9f49574ca4cf57fef0bf63f819c4103e130021
windycityrose <979992760@qq.com>
2017-09-06 01:24:09 +0800

    xiugai5

--- js/details.js
+++ js/details.js
@@ -1,14 +1,17 @@
 /**
  * Created by 李华东 on 2017/9/6.
  */
-function hengpin(){
-    if(window.orientation==90||window.orientation==-90){
-        window.location.reload();
+window.onload=function(){
+    function hengpin(){
+        if(window.orientation==90||window.orientation==-90){
+            window.location.reload();
+
+        }
+        if(window.orientation==0){
+            window.location.reload();
+
+        }
 
     }
-    if(window.orientation==0){
-        window.location.reload();
-
-    }
-
+    window.addEventListener("onorientationchange"  in window  ? "orientationchange" : "resize",hengpin,false);
 }
