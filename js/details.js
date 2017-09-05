752a0200475cfc67388a893a10ecabf46350f1e4
windycityrose <979992760@qq.com>
2017-09-06 01:17:22 +0800

    xiugai4

---
+++ js/details.js
@@ -0,0 +1,14 @@
+/**
+ * Created by 李华东 on 2017/9/6.
+ */
+function hengpin(){
+    if(window.orientation==90||window.orientation==-90){
+        window.location.reload();
+
+    }
+    if(window.orientation==0){
+        window.location.reload();
+
+    }
+
+}
